0d4f01e0bd994fadf380aaaceae59dea181643f5
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-08-03 10:12:43 +0200

    typo in code

--- lib/rubinius/compat/fail.rb
+++ lib/rubinius/compat/fail.rb
@@ -1,7 +1,7 @@
 module Rubinius
   # Others are errors for now
   def const_missing(name)
-    if %w[ByteArray EnvironmentAccess, Type].include? name.to_s
+    if %w[ByteArray EnvironmentAccess Type].include? name.to_s
       raise "Rubinius::#{name} not yet supported under this Ruby impl"
     end
     super
